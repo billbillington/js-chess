f5b819590eb2bbda8fbfb64e27bf5685584d454b
William Billington <william.billington@econsultancy.com>
2015-01-17 22:49:24 +0000

    Move code im messing about with into it's own spec

--- spec/classic_chess_spec.js
+++ spec/classic_chess_spec.js
@@ -22,23 +22,4 @@ describe('Classic Chess', function() {
     //Player 1 turn
     var pieces = board.activePieces({ army: army1 });
   });
-
-  it('moving a peice', function() {
-    var army1 = new ClassicChess.Army();
-    var army2 = new ClassicChess.Army();
-
-    var board = new ClassicChess.Board({
-      army1: army1,
-      army2: army2
-    });
-
-    var whitePawn1 = new army1.Piece('pawn')();
-    var whitePawn2 = new army1.Piece('pawn')();
-
-    var blackPawn1 = new army2.Piece('pawn')();
-
-    board.addPiece({ piece: whitePawn1, row: 0, col: 4 });
-    board.addPiece({ piece: whitePawn2, row: 1, col: 3 });
-    board.addPiece({ piece: blackPawn1, row: 1, col: 5 });
-  });
 });
---
+++ spec/development_spec.js
@@ -0,0 +1,26 @@
+var ClassicChess = require('../classic-chess.js');
+var _ = require('lodash');
+
+describe('Classic Chess', function() {
+  it('moving a peice', function() {
+    var army1 = new ClassicChess.Army();
+    var army2 = new ClassicChess.Army();
+
+    var board = new ClassicChess.Board({
+      army1: army1,
+      army2: army2
+    });
+
+    var whitePawn1 = new army1.Piece('pawn')();
+    var whitePawn2 = new army1.Piece('pawn')();
+
+    var blackPawn1 = new army2.Piece('pawn')();
+
+    board.addPiece({ piece: whitePawn1, row: 0, col: 4 });
+    board.addPiece({ piece: whitePawn2, row: 1, col: 3 });
+    board.addPiece({ piece: blackPawn1, row: 1, col: 5 });
+
+    console.log();
+    console.log(whitePawn1.possibleMoves());
+  });
+});
