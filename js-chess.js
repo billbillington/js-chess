12c05c6955e4c7b8dd7a3825c767e9183b6dcc4e
William Billington <william.billington@econsultancy.com>
2015-01-17 13:34:24 +0000

    Add basic ChessPiece

--- js-chess.js
+++ js-chess.js
@@ -2,6 +2,7 @@ module.exports = function() {
   return {
     ChessBoard: require('./lib/chess_board.js'),
     ChessArmy: require('./lib/chess_army.js'),
+    ChessPiece: require('./lib/chess_peice.js'),
     ClassicChessArmy: require('./lib/classic_chess_army.js'),
     ClassicChessSetup: require('./lib/classic_chess_setup.js')
   };
