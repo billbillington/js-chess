a9145c113e8ff006d6f8e683ecc54f49387c1303
William Billington <william.billington@econsultancy.com>
2015-01-22 00:30:33 +0000

    Add pawn attacks

--- spec/development_spec.js
+++ spec/development_spec.js
@@ -11,7 +11,7 @@ describe('Classic Chess', function() {
       army2: army2
     });
 
-    var attackingPiece = new army1.Piece('rook')();
+    var attackingPiece = new army1.Piece('pawn')();
     var freindlyPiece = new army1.Piece('rook')();
     var enemyPiece = new army2.Piece('rook')();
 
