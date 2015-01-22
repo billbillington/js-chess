3767e5b48128b9c967576a21ec75637a2d31af0c
William Billington <william.billington@econsultancy.com>
2015-01-22 09:59:50 +0000

    Move move into JSChess

--- js-chess.js
+++ js-chess.js
@@ -2,6 +2,7 @@ module.exports = function() {
   return {
     Board: require('./lib/board.js'),
     Army: require('./lib/army.js'),
-    Piece: require('./lib/piece.js')
+    Piece: require('./lib/piece.js'),
+    Move: require('./lib/move.js')
   };
 }();
