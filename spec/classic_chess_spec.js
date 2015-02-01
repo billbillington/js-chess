206f093bbd3057dac9291487b9fbeaea34658cb6
William Billington <william.billington@econsultancy.com>
2015-02-01 14:36:52 +0000

    Add basic end conditions

--- spec/classic_chess_spec.js
+++ spec/classic_chess_spec.js
@@ -4,7 +4,7 @@ var _ = require('lodash');
 
 describe('Classic Chess', function() {
   it('everything', function() {
-    var game = new ClassicChess.Game();
+    var game = new ClassicChess.Game({ turnLimit: 40 });
     var board = game.board();
 
     var guiBoard = new GuiBoard({
@@ -16,13 +16,15 @@ describe('Classic Chess', function() {
     console.log('* Start *');
     guiBoard.print();
 
-    for (var turns = 1; turns <= 40; turns++) {
+    while (game.inProgress()) {
       var turnInfo = game.currentTurnInfo();
+      console.log('- Turn ' + turnInfo.turnNumber() + ' -');
       console.log('* ' + turnInfo.player().name + '\'s move *');
       var piece = _.sample(turnInfo.movablePieces());
       var move  = _.sample(piece.possibleMoves({ board: board }));
       game.makeMove({ move: move });
       guiBoard.print();
     }
+    console.log("** Result: " + game.result() + " **");
   });
 });
