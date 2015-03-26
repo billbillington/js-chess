ac81086b18ac5aec78ee83f8aa664203636bd4c2
William Billington <william.billington@econsultancy.com>
2015-03-26 15:10:29 +0000

    Move setup out of classic chess

--- classic-chess.js
+++ classic-chess.js
@@ -1,11 +1,10 @@
 var JSChess = require('./js-chess.js');
-var Setup = require('./lib/classic_chess/setup.js');
 var Game = require('./lib/classic_chess/game.js');
 
 module.exports = {
   Army: JSChess.Army,
   Board: JSChess.Board,
-  Setup: Setup,
+  Setup: JSChess.Setup,
   Game: Game
 };
 
