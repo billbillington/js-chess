14ed022773c0ad5bcde2564c3ecf70b0a2efe903
William Billington <william.billington@econsultancy.com>
2015-03-26 15:02:51 +0000

    Move Army out of classic chess

--- classic-chess.js
+++ classic-chess.js
@@ -1,10 +1,9 @@
 var JSChess = require('./js-chess.js');
-var Army = require('./lib/classic_chess/army.js');
 var Setup = require('./lib/classic_chess/setup.js');
 var Game = require('./lib/classic_chess/game.js');
 
 module.exports = {
-  Army: Army,
+  Army: JSChess.Army,
   Board: JSChess.Board,
   Setup: Setup,
   Game: Game
