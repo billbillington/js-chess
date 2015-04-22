b92b7f920355eec91bd4c330399f7fe6594d89a4
William Billington <william.billington@econsultancy.com>
2015-04-22 13:24:32 +0100

    wip defender spec

--- spec/check_spec.js
+++ spec/check_spec.js
@@ -42,4 +42,43 @@ describe('check', function() {
 
     expect(army1.inCheck({ board: board })).toEqual(true);
   });
+
+  it('when in check defender can only move if it gets king out of check', function() {
+    var army1 = new JSChess.Army({
+      idGenerator: idGenerator,
+      forwardDirection: 'down'
+    });
+    var army2 = new JSChess.Army({
+      idGenerator: idGenerator,
+      forwardDirection: 'up'
+    });
+
+    var board = new JSChess.Board({
+      idGenerator: idGenerator,
+      army1: army1,
+      army2: army2
+    });
+
+    king = board.addPiece({
+      army: army1,
+      rank: 'king',
+      location: { row: 0, col: 5 }
+    });
+
+    defender = board.addPiece({
+      army: army1,
+      rank: 'rook',
+      location: { row: 0, col: 4 }
+    })
+
+    var attacker = board.addPiece({
+      army: army2,
+      rank: 'queen',
+      location: { row: 5, col: 0 }
+    });
+
+    expect(army1.inCheck({ board: board })).toEqual(true);
+
+    expect(defender.possibleMoves({ board: board }).length).toEqual(1);
+  });
 });
