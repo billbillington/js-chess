291557952e1dd2257688b2d9eec7326372ab4614
William Billington <william.billington@econsultancy.com>
2015-08-07 01:08:35 +0100

    Serialise IDGenerator

--- spec/serialiser_spec.js
+++ spec/serialiser_spec.js
@@ -92,37 +92,42 @@ describe('Serialiser', function() {
 
     it('Serialise Chess board', function(){
       var idGenerator = new IDGenerator();
-      var game = new JSChess.Game({ idGenerator: idGenerator });
+
+      var game = new JSChess.Game({
+        id: idGenerator.nextID(),
+        idGenerator: idGenerator
+      });
 
       var expected = {
         graph: {
-          id: 1,
+          id: 2,
           type: 'Game'
         },
         repository: {
           'Game': {
-            1: {
+            2: {
+              idGenerator: { id: 1, type: 'IDGenerator' },
               player1: {
                 name: 'Player 1',
-                army: { type: 'Army', id: 2 }
+                army: { type: 'Army', id: 3 }
               },
               player2: {
                 name: 'Player 2',
-                army: { type: 'Army', id: 3 }
+                army: { type: 'Army', id: 4 }
               },
               moves: [],
-              board: { type: 'Board', id: 4 }
+              board: { type: 'Board', id: 5 }
             }
           },
           'Army': {
-            2: {
+            3: {
+              idGenerator: { id: 1, type: 'IDGenerator' },
               activePieces: [
-                { id: 5,  type: 'Piece' },
                 { id: 6,  type: 'Piece' },
                 { id: 7,  type: 'Piece' },
                 { id: 8,  type: 'Piece' },
                 { id: 9,  type: 'Piece' },
-                { id: 10, type: 'Piece' },
+                { id: 10,  type: 'Piece' },
                 { id: 11, type: 'Piece' },
                 { id: 12, type: 'Piece' },
                 { id: 13, type: 'Piece' },
@@ -132,15 +137,16 @@ describe('Serialiser', function() {
                 { id: 17, type: 'Piece' },
                 { id: 18, type: 'Piece' },
                 { id: 19, type: 'Piece' },
-                { id: 20, type: 'Piece' }
+                { id: 20, type: 'Piece' },
+                { id: 21, type: 'Piece' }
               ],
               takenPieces: [],
-              king: { type: 'Piece', id: 9 },
+              king: { type: 'Piece', id: 10 },
               forwardDirection: 'down'
             },
-            3: {
+            4: {
+              idGenerator: { id: 1, type: 'IDGenerator' },
               activePieces: [
-                { id: 21, type: 'Piece' },
                 { id: 22, type: 'Piece' },
                 { id: 23, type: 'Piece' },
                 { id: 24, type: 'Piece' },
@@ -155,160 +161,158 @@ describe('Serialiser', function() {
                 { id: 33, type: 'Piece' },
                 { id: 34, type: 'Piece' },
                 { id: 35, type: 'Piece' },
-                { id: 36, type: 'Piece' }
+                { id: 36, type: 'Piece' },
+                { id: 37, type: 'Piece' }
               ],
               takenPieces: [],
-              king: { type: 'Piece', id: 33 },
+              king: { type: 'Piece', id: 34 },
               forwardDirection: 'up'
             }
           },
           'Board': {
-            4: {
+            5: {
               armies: [
-                { id: 2, type: 'Army' },
-                { id: 3, type: 'Army' }
+                { id: 3, type: 'Army' },
+                { id: 4, type: 'Army' }
               ],
               spaces: [
                 [
-                  { id: 5, type: 'Piece' },
                   { id: 6, type: 'Piece' },
                   { id: 7, type: 'Piece' },
                   { id: 8, type: 'Piece' },
                   { id: 9, type: 'Piece' },
                   { id: 10, type: 'Piece' },
                   { id: 11, type: 'Piece' },
-                  { id: 12, type: 'Piece' }
+                  { id: 12, type: 'Piece' },
+                  { id: 13, type: 'Piece' }
                 ],
                 [
-                  { id: 13, type: 'Piece' },
                   { id: 14, type: 'Piece' },
                   { id: 15, type: 'Piece' },
                   { id: 16, type: 'Piece' },
                   { id: 17, type: 'Piece' },
                   { id: 18, type: 'Piece' },
                   { id: 19, type: 'Piece' },
-                  { id: 20, type: 'Piece' }
+                  { id: 20, type: 'Piece' },
+                  { id: 21, type: 'Piece' }
                 ],
                 [],
                 [],
                 [],
                 [],
                 [
-                  { id: 21, type: 'Piece' },
                   { id: 22, type: 'Piece' },
                   { id: 23, type: 'Piece' },
                   { id: 24, type: 'Piece' },
                   { id: 25, type: 'Piece' },
                   { id: 26, type: 'Piece' },
                   { id: 27, type: 'Piece' },
-                  { id: 28, type: 'Piece' }
+                  { id: 28, type: 'Piece' },
+                  { id: 29, type: 'Piece' }
                 ],
                 [
-                  { id: 29, type: 'Piece' },
                   { id: 30, type: 'Piece' },
                   { id: 31, type: 'Piece' },
                   { id: 32, type: 'Piece' },
                   { id: 33, type: 'Piece' },
                   { id: 34, type: 'Piece' },
                   { id: 35, type: 'Piece' },
-                  { id: 36, type: 'Piece' }
+                  { id: 36, type: 'Piece' },
+                  { id: 37, type: 'Piece' }
                 ]
               ]
             }
           },
+          'IDGenerator': {
+            1: { lastID: 37 }
+          },
           'Piece': {
-            5: {
-              army: { id: 2, type: 'Army' },
-              rank: 'rook',
-              isKing: false,
-              moveCount: 0
-            },
             6: {
-              army: { id: 2, type: 'Army' },
-              rank: 'bishop',
+              army: { id: 3, type: 'Army' },
+              rank: 'rook',
               isKing: false,
               moveCount: 0
             },
             7: {
-              army: { id: 2, type: 'Army' },
-              rank: 'knight',
-              isKing: false,
-              moveCount: 0
-            },
-            8: {
-              army: { id: 2, type: 'Army' },
-              rank: 'queen',
-              isKing: false,
-              moveCount: 0
-            },
-            9: {
-              army: { id: 2, type: 'Army' },
-              rank: 'king',
-              isKing: true,
-              moveCount: 0
-            },
-            10: {
-              army: { id: 2, type: 'Army' },
-              rank: 'knight',
-              isKing: false,
-              moveCount: 0
-            },
-            11: {
-              army: { id: 2, type: 'Army' },
+              army: { id: 3, type: 'Army' },
               rank: 'bishop',
               isKing: false,
               moveCount: 0
             },
+            8: {
+              army: { id: 3, type: 'Army' },
+              rank: 'knight',
+              isKing: false,
+              moveCount: 0
+            },
+            9: {
+              army: { id: 3, type: 'Army' },
+              rank: 'queen',
+              isKing: false,
+              moveCount: 0
+            },
+            10: {
+              army: { id: 3, type: 'Army' },
+              rank: 'king',
+              isKing: true,
+              moveCount: 0
+            },
+            11: {
+              army: { id: 3, type: 'Army' },
+              rank: 'knight',
+              isKing: false,
+              moveCount: 0
+            },
             12: {
-              army: { id: 2, type: 'Army' },
-              rank: 'rook',
+              army: { id: 3, type: 'Army' },
+              rank: 'bishop',
               isKing: false,
               moveCount: 0
             },
             13: {
-              army: { id: 2, type: 'Army' },
-              rank: 'pawn',
+              army: { id: 3, type: 'Army' },
+              rank: 'rook',
               isKing: false,
               moveCount: 0
             },
             14: {
-              army: { id: 2, type: 'Army' },
+              army: { id: 3, type: 'Army' },
               rank: 'pawn',
               isKing: false,
               moveCount: 0
             },
             15: {
-               army: { id: 2, type: 'Army' },
-               rank: 'pawn',
-               isKing: false,
-               moveCount: 0
-            },
-            16: {
-              army: { id: 2, type: 'Army' },
+              army: { id: 3, type: 'Army' },
               rank: 'pawn',
               isKing: false,
               moveCount: 0
             },
+            16: {
+               army: { id: 3, type: 'Army' },
+               rank: 'pawn',
+               isKing: false,
+               moveCount: 0
+            },
             17: {
-              army: { id: 2, type: 'Army' },
+              army: { id: 3, type: 'Army' },
               rank: 'pawn',
               isKing: false,
               moveCount: 0
             },
             18: {
-              army: { id: 2, type: 'Army' },
+              army: { id: 3, type: 'Army' },
               rank: 'pawn',
               isKing: false,
               moveCount: 0
             },
             19: {
-              army: { id: 2, type: 'Army' },
+              army: { id: 3, type: 'Army' },
               rank: 'pawn',
               isKing: false,
               moveCount: 0
             },
             20: {
-              army: { id: 2, type: 'Army' },
+              army: { id: 3, type: 'Army' },
               rank: 'pawn',
               isKing: false,
               moveCount: 0
@@ -320,91 +324,97 @@ describe('Serialiser', function() {
               moveCount: 0
             },
             22: {
-              army: { id: 3, type: 'Army' },
+              army: { id: 4, type: 'Army' },
               rank: 'pawn',
               isKing: false,
               moveCount: 0
             },
             23: {
-              army: { id: 3, type: 'Army' },
+              army: { id: 4, type: 'Army' },
               rank: 'pawn',
               isKing: false,
               moveCount: 0
             },
             24: {
-              army: { id: 3, type: 'Army' },
+              army: { id: 4, type: 'Army' },
               rank: 'pawn',
               isKing: false,
               moveCount: 0
             },
             25: {
-              army: { id: 3, type: 'Army' },
+              army: { id: 4, type: 'Army' },
               rank: 'pawn',
               isKing: false,
               moveCount: 0
             },
             26: {
-              army: { id: 3, type: 'Army' },
+              army: { id: 4, type: 'Army' },
               rank: 'pawn',
               isKing: false,
               moveCount: 0
             },
             27: {
-              army: { id: 3, type: 'Army' },
+              army: { id: 4, type: 'Army' },
               rank: 'pawn',
               isKing: false,
               moveCount: 0
             },
             28: {
-              army: { id: 3, type: 'Army' },
+              army: { id: 4, type: 'Army' },
               rank: 'pawn',
               isKing: false,
               moveCount: 0
             },
             29: {
-              army: { id: 3, type: 'Army' },
-              rank: 'rook',
+              army: { id: 4, type: 'Army' },
+              rank: 'pawn',
               isKing: false,
               moveCount: 0
             },
             30: {
-              army: { id: 3, type: 'Army' },
-              rank: 'bishop',
+              army: { id: 4, type: 'Army' },
+              rank: 'rook',
               isKing: false,
               moveCount: 0
             },
             31: {
-              army: { id: 3, type: 'Army' },
-              rank: 'knight',
-              isKing: false,
-              moveCount: 0
-            },
-            32: {
-              army: { id: 3, type: 'Army' },
-              rank: 'queen',
-              isKing: false,
-              moveCount: 0
-            },
-            33: {
-              army: { id: 3, type: 'Army' },
-              rank: 'king',
-              isKing: true,
-              moveCount: 0
-            },
-            34: {
-              army: { id: 3, type: 'Army' },
-              rank: 'knight',
-              isKing: false,
-              moveCount: 0
-            },
-            35: {
-              army: { id: 3, type: 'Army' },
+              army: { id: 4, type: 'Army' },
               rank: 'bishop',
               isKing: false,
               moveCount: 0
             },
+            32: {
+              army: { id: 4, type: 'Army' },
+              rank: 'knight',
+              isKing: false,
+              moveCount: 0
+            },
+            33: {
+              army: { id: 4, type: 'Army' },
+              rank: 'queen',
+              isKing: false,
+              moveCount: 0
+            },
+            34: {
+              army: { id: 4, type: 'Army' },
+              rank: 'king',
+              isKing: true,
+              moveCount: 0
+            },
+            35: {
+              army: { id: 4, type: 'Army' },
+              rank: 'knight',
+              isKing: false,
+              moveCount: 0
+            },
             36: {
-              army: { id: 3, type: 'Army' },
+              army: { id: 4, type: 'Army' },
+              rank: 'bishop',
+              isKing: false,
+              moveCount: 0
+            },
+            37: {
+              army: { id: 4, type: 'Army' },
               rank: 'rook',
               isKing: false,
               moveCount: 0
