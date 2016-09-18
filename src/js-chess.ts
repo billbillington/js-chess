9e1afff753da7d08e16c6384d69f2a111a627972
William Billington <william.billington@gmail.com>
2016-09-19 00:40:51 +0100

    Remove hello world code

--- src/js-chess.ts
+++ src/js-chess.ts
@@ -1,4 +1,3 @@
-import { default as HelloWorld } from './HelloWorld';
 import { default as TwoDimentionalFixedArray } from './two_dimentional_fixed_array';
 
-export { HelloWorld as default, TwoDimentionalFixedArray }
+export { TwoDimentionalFixedArray }
